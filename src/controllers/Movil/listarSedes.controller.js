ac44ef7cc9acd52873978f8c4dff28a45a0f0d13
CristianDaniel <danielblaz59@gmail.com>
2023-04-28 10:46:48 -0500

    Actualizacion de ListarSedes

--- src/controllers/Movil/listarSedes.controller.js
+++ src/controllers/Movil/listarSedes.controller.js
@@ -4,7 +4,7 @@ const getSedes = async(req,res) =>{
     try{
         const connection = await getConnection();
         const result = await connection.query("CALL listar_sedes()");
-        res.json({sedes : result});
+        res.json(result);
     }catch(error){
         res.status(500);
         res.send(error.message);
