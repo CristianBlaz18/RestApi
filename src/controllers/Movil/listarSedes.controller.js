90148baf67663848db7ca55b54b09b06adcab653
CristianDaniel <danielblaz59@gmail.com>
2023-04-28 08:24:57 -0500

    Actualizar Api listar Sedes

--- src/controllers/Movil/listarSedes.controller.js
+++ src/controllers/Movil/listarSedes.controller.js
@@ -3,7 +3,7 @@ import {getConnection} from "../../database/database";
 const getSedes = async(req,res) =>{
     try{
         const connection = await getConnection();
-        const result = await connection.query("call listar_sedes()");
+        const result = await connection.query("CALL listar_sedes()");
         res.json(result);
     }catch(error){
         res.status(500);
