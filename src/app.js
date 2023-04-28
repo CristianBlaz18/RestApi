79a1fb81dcb73c59a4e1c5960b13275c76797837
CristianDaniel <danielblaz59@gmail.com>
2023-04-28 13:23:44 -0500

    Actualizacion de archivo APP

--- src/app.js
+++ src/app.js
@@ -1,20 +1,13 @@
 import express from "express";
 import morgan from "morgan";
 
-//routes
-
-// import languageRoutes from "./routes/language.routes";
-
 import ciclosRoutes from "./routes/Movil/ciclos.routes";
 import listarSedesRoutes from "./routes/Movil/listarSedes.routes";
 import listarCarrerasRoutes from "./routes/Movil/listarCarreras.routes";
 import planEstudioRoutes from "./routes/WebCliente/planEstudio.routes";
 import crearUsuarioRoutes from "./routes/WebAdmin/crearUsuario.routes";
-
 import carreraUsuarioRoutes from "./routes/WebCliente/carreraUsuario.routes";
-
 import validarUsuariosRoutes from "./routes/WebAdmin/validarUsuario.routes";
-
 const app = express();
 
 //setings
@@ -24,23 +17,17 @@ app.set("port", process.env.PORT);
 app.use(morgan("dev"));
 app.use(express.json());
 
-//routes
-
-
-
+//route
 //APP MOVIL
 app.use("/api/listarCiclos",ciclosRoutes);
 app.use("/api/listarSedes",listarSedesRoutes); 
 app.use("/api/listarCarreras",listarCarrerasRoutes);
 
 //WEB CLIENTE
-
 app.use("/api/carreraUsuario",carreraUsuarioRoutes);
 
 //EXPERIENCIA FALTA DEFINIR 
-
 app.use("/api/planEstduio",planEstudioRoutes);
-
 
 //WEB ADMIN
 app.use("/api/validarUsuario",validarUsuariosRoutes);
