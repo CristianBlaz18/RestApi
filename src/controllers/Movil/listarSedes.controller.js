d0c2fd4d3ee17c09212c9170d25fb683f9917b99
CristianDaniel <danielblaz59@gmail.com>
2023-04-28 20:20:02 -0500

    actualizacion Sedes

--- src/controllers/Movil/listarSedes.controller.js
+++ src/controllers/Movil/listarSedes.controller.js
@@ -3,25 +3,22 @@ const getSedes = async(req,res) =>{
     try{
         const connection = await getConnection();
         const [result] = await connection.query("CALL listar_sedes()");
-        
+        const images = {};
         for(let i = 0; i < result.length; i++){
-            const images = {};
+            
             const image = result[i];
             const buffer=Buffer.from(image.blob_sede, 'binary');
             const base64 = buffer.toString('base64');
-            // const response =
-            //     {ID : image.id_sede,
-            //     NombreSede : image.nombre_sede,
-            //     Ruta : image.imagen_sede,                
-            //     Imagen : base64,
-            //     };
-                images.Nombre = image.nombre_sede;
-                images.Id = image.id_sede;
-                images.Ruta = image.imagen_sede;
-                images.Imagen = base64;
-                res.json(images); 
+            const response =
+                {ID : image.id_sede,
+                NombreSede : image.nombre_sede,
+                Ruta : image.imagen_sede,                
+                Imagen : base64,
+                };
+            images[i] = response;
+                
         }
-        
+        res.json(images); 
             
         
 
