5434c3c079f6433ea04a158f94177f46b7cdef41
CristianDaniel <danielblaz59@gmail.com>
2023-04-28 19:45:38 -0500

    Actualizacion

--- src/controllers/Movil/listarSedes.controller.js
+++ src/controllers/Movil/listarSedes.controller.js
@@ -3,43 +3,25 @@ const getSedes = async(req,res) =>{
     try{
         const connection = await getConnection();
         const [result] = await connection.query("CALL listar_sedes()");
-        const images = [];
+        const images = {};
         for(let i = 0; i < result.length; i++){
             const image = result[i];
             const buffer=Buffer.from(image.blob_sede, 'binary');
             const base64 = buffer.toString('base64');
-            const response =
-                {ID : image.id_sede,
-                NombreSede : image.nombre_sede,
-                Ruta : image.imagen_sede,                
-                Imagen : base64,
-                };
-                images.push(response);
+            // const response =
+            //     {ID : image.id_sede,
+            //     NombreSede : image.nombre_sede,
+            //     Ruta : image.imagen_sede,                
+            //     Imagen : base64,
+            //     };
+                images.Nombre = image.NombreSede;
+                images.Id = image.id_sede;
+                images.Ruta = image.imagen_sede;
+                images.Imagen = base64;
+                res.json(images); 
         }
         
-        res.json(images);
-
-        // if(result.length > 0){
-        //     const image = result[0];
-        //     const buffer=Buffer.from(image.blob_sede, 'binary');
-        //     const base64 = buffer.toString('base64');
-        //     const images = [];
-        //     for(let i = 0; i < result.length; i++){
-        //         const response ={Sede : 
-        //             {ID : image.id_sede,
-        //             NombreSede : image.nombre_sede,
-        //             Ruta : image.imagen_sede,                
-        //             Imagen : base64,
-        //             }};
-        //             images.push(response);
-        //     }
             
-        //     res.json(images);
-        // }else{
-        //   res.status(404).send('Image not found');
-        // }
-        
-        
         
 
     }catch(error){
