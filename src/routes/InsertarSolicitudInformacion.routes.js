32c850622d2626ba101391d104bf950eaea95954
CristianDaniel <danielblaz59@gmail.com>
2023-05-19 17:26:23 -0500

    Cambiar APi insertarInformacion a Get

--- src/routes/InsertarSolicitudInformacion.routes.js
+++ src/routes/InsertarSolicitudInformacion.routes.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { methods as insertarSolicitudInformacionController} from "../controllers/Movil/insertarSolicitudInformacion.controller";
 const router = Router();
 
-router.post("/", insertarSolicitudInformacionController.insertarSolicitud); 
+router.get("/", insertarSolicitudInformacionController.insertarSolicitud); 
 
 export default router;
