a64ef343c3797a3f5b43e158ebc17b75fc81ffc0
CristianDaniel <danielblaz59@gmail.com>
2023-05-19 14:39:16 -0500

    Acutalizacion de API

--- src/controllers/Movil/insertarSolicitudInformacion.controller.js
+++ src/controllers/Movil/insertarSolicitudInformacion.controller.js
@@ -1,29 +1,45 @@
-import {getConnection} from "../../database/database";
+import { getConnection } from "../../database/database";
 
-const insertarSolicitud = async(req,res) =>{
-    try{
-        const{id_carrera, nombres, apellido_paterno, apellido_materno, correo, telefono, fecha_nacimiento, modalidad}= req.query;
-        const connection = await getConnection();
-        connection.query(
-            'CALL insertar_solicitud_informacion(?,?,?,?,?,?,?,?)',
-            [id_carrera, nombres, apellido_paterno, apellido_materno, correo, telefono, fecha_nacimiento, modalidad],
-            (error, results, fields) => {
-              if (error) {
-                console.error(error);
-                res.status(500).send('Error al agregar Informacion');
-              } else {
-                res.status(200).send('Solicitud Agregada Correctamente');
-              }
-            }
-          );
-
-    }catch(error){
-        res.status(500);
-        res.send(error.message);
-    }
+const insertarSolicitud = async (req, res) => {
+  try {
+    const {
+      id_carrera,
+      nombres,
+      apellido_paterno,
+      apellido_materno,
+      correo,
+      telefono,
+      fecha_nacimiento,
+      modalidad,
+    } = req.body;
+    const connection = await getConnection();
+    connection.query(
+      "CALL insertar_solicitud_informacion(?,?,?,?,?,?,?,?)",
+      [
+        id_carrera,
+        nombres,
+        apellido_paterno,
+        apellido_materno,
+        correo,
+        telefono,
+        fecha_nacimiento,
+        modalidad,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send("Error al agregar Informacion");
+        } else {
+          res.status(200).send("Solicitud Agregada Correctamente");
+        }
+      }
+    );
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
 };
 
-
 export const methods = {
-    insertarSolicitud
+  insertarSolicitud,
 };
